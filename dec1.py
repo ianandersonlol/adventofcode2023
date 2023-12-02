13c13386a9ca89ff4ecacae12c2816a3ce228f2b
Ian Anderson <ian033190@gmail.com>
2023-12-01 16:12:58 -0800

    Update dec1.py

--- dec1.py
+++ dec1.py
@@ -3,11 +3,11 @@ sum = 0
 with open(file_path, 'r') as file:
     for line in file:
         original_string = line.strip()
-        numbers_only_string = "".join([char for char in original_string if char.isdigit()])
+        numbers_only_string = "".join([i for i in original_string if i.isdigit()])
         first_digit = numbers_only_string[0]
         last_digit = numbers_only_string[-1]
         answer = first_digit + last_digit
-        print(answer)
+        #print(answer)
         sum = sum + int(answer)
 
 print(sum)
